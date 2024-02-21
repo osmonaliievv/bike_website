f9f0addc0cad26391ed9c1e30b5c25af7c0bd0f0
Osmonaliev Adilnur <osmonaliievv@gmail.com>
2024-02-21 11:51:18 +0300

    header-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import "./Footer.scss";
+import icon from "../../img/icons/icon.svg";
+import phone from "../../img/footer-icons/phone.svg";
+import navigation from "../../img/footer-icons/navigation.svg";
+import mail from "../../img/footer-icons/mail.svg";
 
 export default function Footer() {
-	return (
-		<footer className="footer">
-			<div className="footer__container"></div>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__top">
+          <div className="footer-logo">
+            <img src={icon} alt="logo" />
+          </div>
+        </div>
+        <div className="blocks">
+          <ul className="block__list">
+            <li className="block">Каталог</li>
+            <li className="block__item">Велосипеды</li>
+            <li className="block__item">Аксессуары</li>
+          </ul>
+          <ul className="block__list">
+            <li className="block">Для клиента</li>
+            <li className="block__item">О нас</li>
+            <li className="block__item">Доставка и оплата</li>
+            <li className="block__item">Блог</li>
+            <li className="block__item">Веломастерская</li>
+            <li className="block__item">Хранение</li>
+            <li className="block__item">Гарантии</li>
+          </ul>
+          <ul className="block__list">
+            <li className="block">Контакты</li>
+            <li className="block__item">
+              <img className="f-m-i" src={phone} alt="phone" />
+              +7(495)055-75-86
+            </li>
+            <li className="block__item">
+              <img className="f-m-i" src={navigation} alt="navigation" />
+              г. Москва, ул. Доватора, 7/8 с1
+            </li>
+            <li className="block__item">
+              <img className="f-m-i" src={mail} alt="mail" />
+              order@world-bike.ru
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
-import './Header.scss';
-import '../../styles/common.scss';
+import React, { useState, useEffect } from "react";
+import "./Header.scss";
+import icon from "../../img/icons/icon.svg";
+import search from "../../img/icons/search.svg";
+import contact from "../../img/icons/contact.svg";
+import heart from "../../img/icons/heart.svg";
+import corzina from "../../img/icons/corzina.svg";
+import burger_menu from "../../img/icons/burger-menu.svg";
 
 export default function Header() {
-	return (
-		<header className="header">
-			<div className="header__container"></div>
-		</header>
-	);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header-logo" onClick={toggleSidebar}>
+          <img src={icon} alt="logo" />
+        </div>
+        {(isMobile && menuVisible) || !isMobile ? (
+          <div className="header-menu">
+            <div className="header-div">TRADE IN</div>
+            <div className="header-div">Велосипеды</div>
+            <div className="header-div">Аксессуары</div>
+          </div>
+        ) : null}
+        <ul className="header-icon">
+          <li>
+            <img src={search} alt="search" />
+          </li>
+          <li>
+            <img src={contact} alt="contact" />
+          </li>
+          <li>
+            <img src={heart} alt="heart" />
+          </li>
+          <li>
+            <img src={corzina} alt="corzina" />
+          </li>
+        </ul>
+        {isMobile && (
+          <div className="burger-menu" onClick={toggleMenu}>
+            <img src={burger_menu} alt="menu" />
+          </div>
+        )}
+        <div className={`sidebar ${sidebarVisible ? "open" : ""}`}>
+          <ul className="sidebar-menu">
+            <li className="sidebar-item">Главная</li>
+            <li className="sidebar-item">Товары</li>
+            <li className="sidebar-item">О нас</li>
+            <li className="sidebar-item">Контакты</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 }
